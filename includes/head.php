83eaa6da4508bba7c0aa7afad4e11135a7eccd2d
ccalash98 <cristtofher.ccalash@gmail.com>
2025-04-24 15:25:18 -0500

    sinc

---
+++ includes/head.php
@@ -0,0 +1,38 @@
+<head>
+   <!-- basic -->
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <!-- mobile metas -->
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <!-- site metas -->
+   <title>Monterreal</title>
+   <meta name="keywords" content="">
+   <meta name="description" content="Proyectos Inmobiliarios, Porque te mereces tener tu PROPIO TERRENO, te acompañamos a celebrar tu nuevo hogar.">
+   <meta name="author" content="Cristtofher Ccalash">
+   <!-- bootstrap css -->
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+   <!-- style css -->
+   <link rel="stylesheet" href="css/style.css">  
+   <!-- Responsive-->
+   <link rel="stylesheet" href="css/responsive.css">
+
+
+   <link rel="stylesheet" href="css/owl.carousel.min.css">  
+   <!-- fevicon -->  
+   <link rel="icon" href="images/fevicon.png" type="image/gif" />
+   <link rel="icon" href="./images/logo_mini.png" sizes="32x32" />
+   <!-- Scrollbar Custom CSS -->
+   <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+   <link rel="stylesheet" href="css/main.css">
+
+   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
+   <!-- Tweaks for older IEs -->
+   <!--[if lt IE 9]>
+         <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->     
+   <!-- Font Awesome -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+   <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+</head>
--- service.php
+++ service.php
@@ -1,49 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-   <!-- basic -->
-   <meta charset="utf-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <!-- mobile metas -->
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <!-- site metas -->
-   <title>Monterreal</title>
-   <meta name="keywords" content="">
-   <meta name="description" content="Proyectos Inmobiliarios, Porque te mereces tener tu PROPIO TERRENO, te acompañamos a celebrar tu nuevo hogar.">
-   <meta name="author" content="Cristtofher Ccalash">
-   <!-- bootstrap css -->
-   <link rel="stylesheet" href="css/bootstrap.min.css">
-   <!-- style css -->
-   <link rel="stylesheet" href="css/style.css">  
-   <!-- Responsive-->
-   <link rel="stylesheet" href="css/responsive.css">
-
-
-   <link rel="stylesheet" href="css/owl.carousel.min.css">  
-   <!-- fevicon -->  
-   <link rel="icon" href="images/fevicon.png" type="image/gif" />
-   <link rel="icon" href="./images/logo_mini.png" sizes="32x32" />
-   <!-- Scrollbar Custom CSS -->
-   <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
-   <link rel="stylesheet" href="css/main.css">
-
-   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
-   <!-- Tweaks for older IEs -->
-   <!--[if lt IE 9]>
-         <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-      <![endif]-->     
-   <!-- Font Awesome -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-   <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css"
-   
-      media="screen">
-</head>
+<!-- head -->
+<?php include 'includes/head.php'; ?>
 
 <!-- body -->
-
 <body class="main-layout"> 
    <!-- loader  -->    
   <div class="loader_bg">  
@@ -69,19 +29,19 @@
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav ml-auto">
                             <li class="nav-item">
-                                <a class="nav-link active" href="index.htm" onclick="hideNavbar()">INICIO</a>
+                                <a class="nav-link active" href="index.php" onclick="hideNavbar()">INICIO</a>
                             </li>
                             <li class="nav-item">  
-                                <a class="nav-link" href="proyectos.htm" onclick="hideNavbar()">NUESTROS PROYECTOS</a>
+                                <a class="nav-link" href="proyectos.php" onclick="hideNavbar()">NUESTROS PROYECTOS</a>
                             </li>
                             <li class="nav-item ">
-                                <a class="nav-link" href="nosotros.htm" onclick="hideNavbar()">NOSOTROS</a>
+                                <a class="nav-link" href="nosotros.php" onclick="hideNavbar()">NOSOTROS</a>
                             </li>       
                             <li class="nav-item">
-                                <a class="nav-link" href="info.htm" onclick="hideNavbar()">INFORMACION</a>
+                                <a class="nav-link" href="info.php" onclick="hideNavbar()">INFORMACION</a>
                             </li>  
                             <li class="nav-item">
-                                <a class="nav-link" href="index.htm" onclick="hideNavbar()">TRABAJA CON NOSOTROS</a> 
+                                <a class="nav-link" href="index.php" onclick="hideNavbar()">TRABAJA CON NOSOTROS</a> 
                             </li>
                         </ul>
                     </div>
